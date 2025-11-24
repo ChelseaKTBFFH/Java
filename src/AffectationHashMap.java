b0b027b24780d805e44d8547c1a9eece9b80b996
ChelseaKTBFFH <youssef.zrelliesprit.tn>
2025-11-24 11:23:08 +0100

    Prosit11

--- src/AffectationHashMap.java
+++ src/AffectationHashMap.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AffectationHashMap {
+
+    private HashMap<Employe, Departement> map = new HashMap<>();
+
+    // Instruction 2
+    public void ajouterEmployeDepartement(Employe e, Departement d) {
+        map.put(e, d);
+    }
+
+    // Instruction 3
+    public void afficherEmployesEtDepartements() {
+        for (Map.Entry<Employe, Departement> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " --> " + entry.getValue());
+        }
+    }
+
+    // Instruction 4
+    public void supprimerEmploye(Employe e) {
+        map.remove(e);
+    }
+
+    // Instruction 5
+    public void supprimerEmployeEtDepartement(Employe e, Departement d) {
+        if (map.containsKey(e) && map.get(e).equals(d)) {
+            map.remove(e);
+        }
+    }
+
+    // Instruction 6
+    public void afficherEmployes() {
+        for (Employe e : map.keySet()) {
+            System.out.println(e);
+        }
+    }
+
+    // Instruction 7
+    public void afficherDepartements() {
+        for (Departement d : map.values()) {
+            System.out.println(d);
+        }
+    }
+
+    // Instruction 8
+    public boolean rechercherEmploye(Employe e) {
+        return map.containsKey(e);
+    }
+
+    // Instruction 9
+    public boolean rechercherDepartement(Departement d) {
+        return map.containsValue(d);
+    }
+
+    // Instruction 10
+    public TreeMap<Employe, Departement> trierMap() {
+        return new TreeMap<>(map);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Gestion des employés
+
         SocieteArrayList societe = new SocieteArrayList();
         Employe e1 = new Employe(1, "Ali", "Karoui", "IT", 3);
         Employe e2 = new Employe(3, "Sana", "Ben Ali", "RH", 2);
@@ -11,18 +11,21 @@ public class Main {
         societe.ajouterEmploye(e2);
         societe.ajouterEmploye(e3);
 
-        System.out.println("Liste initiale des employés :");
+        System.out.println("===== LISTE INITIALE DES EMPLOYES =====");
         societe.displayEmploye();
 
-        System.out.println("\nTri par ID :");
+        System.out.println("\n===== TRI PAR ID =====");
         societe.trierEmployeParId();
         societe.displayEmploye();
 
-        System.out.println("\nTri par Nom, Département, Grade :");
+        System.out.println("\n===== TRI PAR NOM, DEPARTEMENT, GRADE =====");
         societe.trierEmployeParNomDépartementEtGrade();
         societe.displayEmploye();
 
-        // Gestion des départements
+
+        // ============================================
+        //     GESTION DES DEPARTEMENTS (HashSet)
+        // ============================================
         DepartementHashSet depHash = new DepartementHashSet();
         Departement d1 = new Departement(2, "IT");
         Departement d2 = new Departement(1, "RH");
@@ -32,12 +35,53 @@ public class Main {
         depHash.ajouterDepartement(d2);
         depHash.ajouterDepartement(d3);
 
-        System.out.println("\nListe des départements :");
+        System.out.println("\n===== LISTE DES DEPARTEMENTS =====");
         depHash.displayDepartement();
 
-        System.out.println("\nDépartements triés par ID :");
+        System.out.println("\n===== DEPARTEMENTS TRIES PAR ID =====");
         for (Departement d : depHash.trierDepartementById()) {
             System.out.println(d);
         }
+
+
+        System.out.println("\n===== AFFECTATION DES EMPLOYES AUX DEPARTEMENTS =====");
+
+        AffectationHashMap affect = new AffectationHashMap();
+
+        affect.ajouterEmployeDepartement(e1, d1);
+        affect.ajouterEmployeDepartement(e2, d2);
+        affect.ajouterEmployeDepartement(e3, d1);
+
+        System.out.println("\n--- Affichage initial des affectations ---");
+        affect.afficherEmployesEtDepartements();
+
+        // TEST: ajouter le même employé dans un autre département
+        affect.ajouterEmployeDepartement(e1, d3);
+
+        System.out.println("\n--- Après modification du département de e1 ---");
+        affect.afficherEmployesEtDepartements();
+
+        // Test des méthodes supplémentaires
+        System.out.println("\n===== RECHERCHE =====");
+        System.out.println("Employe e2 existe ? " + affect.rechercherEmploye(e2));
+        System.out.println("Departement Finance existe ? " + affect.rechercherDepartement(d3));
+
+        System.out.println("\n===== LISTE DES EMPLOYES SEULEMENT =====");
+        affect.afficherEmployes();
+
+        System.out.println("\n===== LISTE DES DEPARTEMENTS SEULEMENT =====");
+        affect.afficherDepartements();
+
+        // Test suppression
+        System.out.println("\n===== SUPPRESSION e3 =====");
+        affect.supprimerEmploye(e3);
+        affect.afficherEmployesEtDepartements();
+
+        System.out.println("\n===== TRI DE LA MAP PAR ID D’EMPLOYE =====");
+        for (var entry : affect.trierMap().entrySet()) {
+            System.out.println(entry.getKey() + " --> " + entry.getValue());
+        }
+
+        System.out.println("\n===== FIN DU PROGRAMME =====");
     }
 }
